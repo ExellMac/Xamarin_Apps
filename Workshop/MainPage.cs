4cb7872a6c61ce3e67466cf87314f639ccc93a53
Butcher <81714724+ExellMac@users.noreply.github.com>
2023-08-18 15:50:55 -0400

    Create MainPage.cs

---
+++ Workshop/MainPage.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ferreteria
+{
+    [Obsolete]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Master = new Master();
+            this.Detail = new NavigationPage(new Details());
+            App.MasterDet = this;
+
+        }
+    }
+}
